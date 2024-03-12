b590004d9650ef38fe76696305b6e3889dd0fa72
Somgester <dhruvratmele13@gmail.com>
2024-03-12 20:36:21 +0530

    initial commit

--- src/SudokuClass.java
+++ src/SudokuClass.java
@@ -1,11 +1,29 @@
 import java.util.*;
 import java.io.*;
-import javax.swing.*;
+import javax.swing.JPanel;
 import java.awt.*;
 import java.awt.event.*;
 
 
-public class SudokuClass {
+public class SudokuClass extends JPanel {
+
+
+    public SudokuClass(int width, int height)
+    {
+        //set up the game
+        setPreferredSize(new Dimension(width, height));
+        setFocusable(true);
+        requestFocus();
+        addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent e) {
+                int keyCode = e.getKeyCode();
+                if (keyCode == KeyEvent.VK_ESCAPE) {
+                    System.exit(0);
+                }
+            }
+        });
+    }
+
     public static void main(String[] args)
     {
         
