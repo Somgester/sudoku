416ff50346606e989bb05c4d4a0095683d8de018
Somgester <dhruvratmele13@gmail.com>
2024-03-18 16:36:35 +0530

    made some changes

--- src/SudokuClass.java
+++ src/SudokuClass.java
@@ -7,7 +7,18 @@ import java.awt.event.*;
 
 public class SudokuClass extends JPanel {
 
+    private class Tile {
+        int x;
+        int y;
 
+        public Tile(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    int width;
+    int height;
+    int tileSize = 66;
     public SudokuClass(int width, int height)
     {
         //set up the game
@@ -23,9 +34,25 @@ public class SudokuClass extends JPanel {
             }
         });
     }
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
 
+    private void draw(Graphics g) {
+        // grids on the board
+        // x1,y1,x2,y2
+        for (int i = 0; i < width / tileSize; i++) {
+            g.drawLine(i * tileSize, 0, i * tileSize, height);
+            g.drawLine(0, i * tileSize, width, i * tileSize);
+            g.setColor(Color.BLACK);
+        }
+    }
     public static void main(String[] args)
     {
+        // Use the Tile constructor
+        SudokuClass sudokuClass = new SudokuClass(500, 500);
+        Tile tile = sudokuClass.new Tile(66, 66);
         
     }
 }
